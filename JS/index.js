ed18b355fd7c67761be1dad4efd651a41d4fa725
Hieu Nguyen <hnguye51@uw.edu>
2016-11-02 14:41:19 -0700

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -7,7 +7,7 @@ $(document).on("click", "#joinroom", function(data) {
     else
     {
         setLocalName();
-        location.href = "HTML/rooms.html";
+        location.href = "./HTML/rooms.html";
     }
 });
 
@@ -21,7 +21,6 @@ $(document).on("click", "#hostroom", function(data) {
     {
         setLocalName();
         addRoom();
-        location.href = "HTML/Host.html";
     }
 });
 
@@ -58,4 +57,5 @@ function addRoom()
     
     newRoom.set(newRoomData);
     console.log("Got here");
+    location.href = "./HTML/Host.html";
 }
